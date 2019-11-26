3ebb5adc8aff69bd9ec28d30d3b198007799e710
bertRC <sithalbert@ya.ru>
2019-11-26 21:07:57 +0300

    ResourcesPaths added

---
+++ web-app/src/main/java/ru/itpark/web/util/ResourcesPaths.java
@@ -0,0 +1,13 @@
+package ru.itpark.web.util;
+
+public class ResourcesPaths {
+    public static final String uploadPath = "UPLOAD_PATH";
+    public static final String dbPath = "java:/comp/env/jdbc/db";
+    public static final String frontpageJsp = "/WEB-INF/frontpage.jsp";
+    public static final String detailsJsp = "/WEB-INF/details.jsp";
+    public static final String errorJsp = "/WEB-INF/404.jsp";
+    public static final String searchResultsJsp = "/WEB-INF/search-results.jsp";
+
+    private ResourcesPaths() {
+    }
+}
