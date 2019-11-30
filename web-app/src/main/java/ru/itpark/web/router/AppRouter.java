c40f5dcbd795ccf74e8ccbe7674277f5cc08456b
bertRC <sithalbert@ya.ru>
2019-11-30 22:04:59 +0300

    trying to catch demo files from app/upload directory

--- web-app/src/main/java/ru/itpark/web/router/AppRouter.java
+++ web-app/src/main/java/ru/itpark/web/router/AppRouter.java
@@ -41,8 +41,8 @@ public class AppRouter implements Router {
             AutoModel moskvitchAuto = new AutoModel(0, "Moskvitch-412", "", "MOSKVITCH-412-DEMO");
             autoService.save(kopeykaAuto, null);
             autoService.save(moskvitchAuto, null);
-            fileService.copyFileFromUrl("https://sun9-3.userapi.com/c857216/v857216796/66d14/xVq-eHIvPrU.jpg", "VAZ-2103-DEMO");
-            fileService.copyFileFromUrl("https://sun9-36.userapi.com/c857216/v857216796/66d0d/i2nKfr2jif4.jpg", "MOSKVITCH-412-DEMO");
+//            fileService.copyFileFromUrl("https://sun9-3.userapi.com/c857216/v857216796/66d14/xVq-eHIvPrU.jpg", "VAZ-2103-DEMO");
+//            fileService.copyFileFromUrl("https://sun9-36.userapi.com/c857216/v857216796/66d0d/i2nKfr2jif4.jpg", "MOSKVITCH-412-DEMO");
         } catch (NamingException e) {
             throw new InitializationException(e);
         }
